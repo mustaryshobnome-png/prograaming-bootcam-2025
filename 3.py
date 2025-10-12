df3a41f5fef06f55f0c12f350fecf07d68b853ca
mustaryshobnome-png <mustary.shobnome@gmail.com>
2025-10-12 16:14:33 +0300

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,10 @@
+
+#probelm statement:
+'''Add two numbers and print the result'''
+
+#solution:
+a = int(input('give your first number:'))
+
+b = int(input('give your first number:'))
+
+print(f'The result is:{a+b}')
