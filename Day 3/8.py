41b0baee7eb18e5c26225f2c3f8e1f015058f53c
mustaryshobnome-png <mustary.shobnome@gmail.com>
2025-10-26 18:06:20 +0200

    Create 8.py

---
+++ Day 3/8.py
@@ -0,0 +1,10 @@
+#probelm statement:
+'''Check if string is empty'''
+
+#soliution:
+text = input("give some input ")
+
+if len(text) == 0:
+    print('string is emty')
+else:
+    print('string is not emty')    
