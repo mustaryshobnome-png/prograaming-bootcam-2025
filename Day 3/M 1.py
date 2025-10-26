dc29b0036540450ed805c4557e2c70c0fbc19a0f
mustaryshobnome-png <mustary.shobnome@gmail.com>
2025-10-26 19:03:17 +0200

    Create M 1.py

---
+++ Day 3/M 1.py
@@ -0,0 +1,16 @@
+#probelm statement:
+'''Grade system: A(>=90), B(>=80), C(>=70), D(>=60), F'''
+
+#soliution:
+grade=85
+if grade >= 90:
+  print("Grade: A")
+elif grade >= 80:
+  print("Grade: B")
+elif grade >= 70:
+  print("Grade: C")
+elif grade >= 60:
+  print("Grade: D")
+elif grade >= 30:
+  print("Grade: F")
+  
