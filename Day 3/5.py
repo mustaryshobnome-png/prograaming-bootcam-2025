6c3392f3de423a93396e9ce7a1adb2b196838c44
mustaryshobnome-png <mustary.shobnome@gmail.com>
2025-10-26 16:51:27 +0200

    Create 5.py

---
+++ Day 3/5.py
@@ -0,0 +1,9 @@
+#probelm statement:
+'''Check if char is vowel (a, e, i, o, u)'''
+
+#soliution:
+char = input("give me a letter")
+if char in ("a","e","i","o","u"):
+    print("vowel")
+else:
+    print("consonent")    
