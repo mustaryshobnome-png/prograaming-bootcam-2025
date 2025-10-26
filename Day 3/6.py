b4e1bcb22cb79044e8ca4d23d4730e4b1212b4c8
mustaryshobnome-png <mustary.shobnome@gmail.com>
2025-10-26 17:22:44 +0200

    Create 6.py

---
+++ Day 3/6.py
@@ -0,0 +1,9 @@
+#probelm statement:
+'''Check if number divisible by 5'''
+
+#soliution:
+number = int(input("give me a number"))
+if number%5 == 0:
+    print("the number is divisible")
+else:
+    print("the number is not divisible") 
