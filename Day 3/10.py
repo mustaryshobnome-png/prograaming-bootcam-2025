348633e697597c3e4b64b63468f9b8b66cd66a6a
mustaryshobnome-png <mustary.shobnome@gmail.com>
2025-10-26 18:40:30 +0200

    Create 10.py

---
+++ Day 3/10.py
@@ -0,0 +1,10 @@
+#probelm statement:
+'''Check if password is "secret"'''
+
+#soliution:
+password=input('enter password: ')
+
+if password:
+    print("Login successfully")
+else:
+   print('it will be incorrect')
